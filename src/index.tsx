4ba8ac6558c5d4ecf6675910ab871fd8bea388cb
Eugene Petrushenko <e.petrushenko@paysenger.com>
2022-09-19 11:40:51 +0300

    Added styles (colors, layers, medias)

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
-import './index.css';
+import 'normalize.css';
+import './styles/index.scss';
 import App from './app/App';
 import reportWebVitals from './reportWebVitals';
 import { store } from './store/store';
